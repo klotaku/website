d9ecca7e398198b9a2c517d5dee344fc652b1c8a
chloe <107429634+klotaku@users.noreply.github.com>
2024-01-07 22:44:14 +0000

    create

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import './App.css';
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      once: true
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
